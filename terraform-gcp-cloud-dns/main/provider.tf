26be96c8ce46e55cc9578cd4fe18fe3f6d3373f4
Ritesh Kumar Singh <56765895+singhritesh85@users.noreply.github.com>
2025-07-18 11:30:36 +0530

    Update provider.tf

--- terraform-gcp-cloud-dns/main/provider.tf
+++ terraform-gcp-cloud-dns/main/provider.tf
@@ -2,7 +2,3 @@ provider "google" {
   project = var.project_name  ### Provide Project ID for your GCP Account
   region  = "us-central1"    ###var.gcp_region[1]
 }
-
-provider "aws" {
-  region = var.region
-}
