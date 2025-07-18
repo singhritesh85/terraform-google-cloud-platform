9b18708ac53ad70d08c63c9cf2fa60ba67f82a64
Ritesh Kumar Singh <56765895+singhritesh85@users.noreply.github.com>
2025-07-18 14:03:24 +0530

    Delete terraform-gcp-regional-autoscale-application-loadbalancer directory

--- terraform-gcp-regional-autoscale-application-loadbalancer/main/backend.tf
+++
@@ -1,6 +0,0 @@
-terraform {
-  backend "gcs" {
-    bucket  = "dolo-dempo"
-    prefix  = "regional-state/autoscale-alb"
-  }
-}
--- terraform-gcp-regional-autoscale-application-loadbalancer/main/provider.tf
+++
@@ -1,4 +0,0 @@
-provider "google" {
-  project = var.project_name  ### Provide Project ID for your GCP Account
-  region  = "us-central1"    ###var.gcp_region[1]
-}
